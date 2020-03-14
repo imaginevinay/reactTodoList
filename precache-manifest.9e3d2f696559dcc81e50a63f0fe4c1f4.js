60e33485c3c705a16a03d23dbb8792307a6c0083
imagineVinay <vinay.varshney28@gmail.com>
2020-03-15 02:35:44 +0530

    Updates

---
+++ precache-manifest.9e3d2f696559dcc81e50a63f0fe4c1f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1dcdfa0ff9eadfc0d09381c1af68ed",
+    "url": "/reactTodoList/index.html"
+  },
+  {
+    "revision": "f1f70d1770117de976a5",
+    "url": "/reactTodoList/static/css/main.d6329276.chunk.css"
+  },
+  {
+    "revision": "0fcc40324f5aabdfe886",
+    "url": "/reactTodoList/static/js/2.8e790e84.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/reactTodoList/static/js/2.8e790e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f70d1770117de976a5",
+    "url": "/reactTodoList/static/js/main.cf9ae23a.chunk.js"
+  },
+  {
+    "revision": "440a826bfa1bb1f198da",
+    "url": "/reactTodoList/static/js/runtime-main.f6fbd962.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactTodoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
